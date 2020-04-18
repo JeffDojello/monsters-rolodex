1bb5706ff3be8a7e57326567949bd109543e6405
JeffDojello <dojellomj@yahoo.com>
2020-04-19 03:00:00 +0800

    Updates

---
+++ precache-manifest.ee575931e92afad4166de95b385901ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acfd87d611f1eee5b457d70750617b34",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8b3717f6984c25c08ff5",
+    "url": "/monsters-rolodex/static/css/main.26ad47e1.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b3717f6984c25c08ff5",
+    "url": "/monsters-rolodex/static/js/main.77c92a13.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
